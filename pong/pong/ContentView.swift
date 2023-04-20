cb0aa619cf18570e2f0da71b73cc2627056dd0f1
rutger <rutgerzels@live.nl>
2023-04-20 19:14:52 +0200

    hello world verwijdert

--- pong/pong/ContentView.swift
+++ pong/pong/ContentView.swift
@@ -1,3 +1,4 @@
+
 //
 //  ContentView.swift
 //  pong
@@ -10,10 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Circle()
+                        
         }
         .padding()
     }
