5feaac567fb2f9e6323e9cd547b656a6dfd77420
rutger <rutgerzels@live.nl>
2023-04-20 19:04:42 +0200

    initieel committee

---
+++ pong/pong/pongApp.swift
@@ -0,0 +1,17 @@
+//
+//  pongApp.swift
+//  pong
+//
+//  Created by Rutger Verweij on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct pongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
